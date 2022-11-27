908d2028773892b782890b2fdd78c9b10ec5497f
Przemek Ferkaluk <pferkaluk@gmail.com>
2022-11-27 10:33:45 +0100

    controllers

---
+++ server/controllers/order_controller.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Order = require('../models/order_schema');
+
+const createData = (req, res) => {
+  Order.create(req.body)
+    .then((data) => {
+      console.log('New Order Created!', data);
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
+};
+
+const readData = (req, res) => {
+  Order.find()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
+
+const updateData = (req, res) => {
+  Order.findByIdAndUpdate(req.params.id, req.body, {
+    useFindAndModify: false,
+    new: true,
+  })
+    .then((data) => {
+      console.log('Order updated!');
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
+};
+
+const deleteData = (req, res) => {
+    Order.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        throw new Error('Order not available');
+      }
+      return data.remove();
+    })
+    .then((data) => {
+      console.log('Order removed!');
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
+
+module.exports = {
+  createData,
+  readData,
+  updateData,
+  deleteData,
+};
---
+++ server/controllers/variant_controller.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Variant = require('../models/variant_schema');
+
+const createData = (req, res) => {
+    Variant.create(req.body)
+    .then((data) => {
+      console.log('New Variant Created!', data);
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
+};
+
+const readData = (req, res) => {
+    Variant.find()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
+
+const updateData = (req, res) => {
+    Variant.findByIdAndUpdate(req.params.id, req.body, {
+    useFindAndModify: false,
+    new: true,
+  })
+    .then((data) => {
+      console.log('Variant updated!');
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
+};
+
+const deleteData = (req, res) => {
+    Variant.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        throw new Error('Variant not available');
+      }
+      return data.remove();
+    })
+    .then((data) => {
+      console.log('Variant removed!');
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
+
+module.exports = {
+  createData,
+  readData,
+  updateData,
+  deleteData,
+};
